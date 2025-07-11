149e8c50aa9c19477074d03d903c2dd894b51dad
Kartik Kumar <kartikjhaba@gmail.com>
2025-07-11 15:41:30 +0530

    tailwind config and props

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,13 @@
+import Card from "./components/Card"
+
+function App() {
+   return (
+    <>
+    <h1>Hello world</h1>
+    <Card title="Black chocolate" price = {100}/>
+    <Card title="White chocolate" />
+     </>
+  )
+}
+
+export default App
