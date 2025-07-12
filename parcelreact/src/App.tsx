13f1a53328a5259385e922e3fede2402ad1c357f
Kartik Kumar <kartikjhaba@gmail.com>
2025-07-12 13:23:17 +0530

    project with parcel

---
+++ parcelreact/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className='text-2xl'>Parcel with React App</h1>
+      <h1 className='bg-red-500'>Hola shola</h1>
+    </>
+  );
+}
+
+export default App
---
+++ parcelreact/src/index.tsx
@@ -0,0 +1,11 @@
+import { createRoot } from 'react-dom/client';
+import { StrictMode } from 'react';
+import App from './App';
+
+
+createRoot(document.getElementById("app")!)
+.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+);
